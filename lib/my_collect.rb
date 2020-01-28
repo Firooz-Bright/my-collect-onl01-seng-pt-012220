ea606c1a0561ef8982148e7d9d1536b3ae6c2c68
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-28 00:19:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   i=1
-   collect=[]
+   final=[]
     while i< array.length
-    collect.push(yield array[i])
+    final.push(yield array[i])
     i+=1 
  end 
- return collect 
+ return final  
 end 
 
